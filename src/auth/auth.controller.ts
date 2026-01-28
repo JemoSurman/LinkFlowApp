f05a4c402eb92e8d990ce013225cb16481c562af
Jemo Surman <jemosurman@Jemos-MacBook-Pro.local>
2026-01-28 21:20:07 +0400

    Setup Docker, Prisma and initial Auth structure

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth') 
 export class AuthController{
     constructor(private authService : AuthService){
-        
-
     }
     @Post('signup')
-    signup(){
+    signup(@Body() dto: any){
+        console.log({
+            dto,
+        })
         return this.authService.signup();
     }
     @Post('signin')
