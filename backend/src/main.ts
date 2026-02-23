b7631e3b2e0b46d708d243682031e6d12257fb32
JemoSurman <jemosurman@gmail.com>
2026-02-23 12:54:24 +0400

    fix: allow CORS for Vercel deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://link-flow-app.vercel.app/login', 
+    credentials: true,
+});
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));  
